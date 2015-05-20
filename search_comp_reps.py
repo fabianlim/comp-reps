3b64eac1ae4b48c5cc49706c013becf51c30b174
fabianlim <fabian.lim@gmail.com>
2015-05-19 21:54:06 -0700

    two experiments in experiments.py - first one sort of a brute force enumeration     - coefficient assignment is inefficient, tries to assign pairs and singles for now - second one sort of uses some groebner basis information to speed up search     - intuition is to populate the leaves using the groebner remainder

--- search_comp_reps.py
+++ search_comp_reps.py
@@ -1,8 +1,8 @@
 # some hacking to flesh out ideas on searching equivalent comp graphs
 
 from sympy import reduced, Poly, groebner
-from itertools import combinations
 from re import match
+import time
 
 
 # range_var
@@ -21,40 +21,12 @@ def str_poly(p):
         return ""
 
 
-# backtrack
-def backtrack(function, X, num_max_Y_variates=6):
+# builder
+def search_routine(function, initial_node, extensions):
     """
         This routine searches through a tree hierachy of comp representations.
         function: the function you want to compute
-        X: set of X variates
     """
-
-    # polys (shortcut)
-    def pairs(a, b):
-        " will only consider these polynomial pairs for now "
-        return ['{} + {}'.format(a, b), '{} - {}'.format(a, b)]
-
-    leaves = [i for x in combinations(X, 2) for i in pairs(*x)]
-
-    # extensions
-    def extensions(g, a, b):
-        """ extend the array of polys g
-            a: number of Y variates already assigned
-            b: number of Y variates
-        """
-
-        # choice of Y
-        if b+2 <= num_max_Y_variates:
-            # this would
-            connector = [[Poly('y{} - y{} - y{}'.format(b, b+1, b+2)), a, b+2]]
-        else:
-            connector = []
-
-        # choices of assigning Ya
-        choices = ([[Poly('y{} - ({})'.format(a+1, i)), a+1, b]
-                   for i in leaves] + connector)
-
-        return [x for x in choices if x not in g]
 
     # search
     def search(H, n, m):
@@ -69,8 +41,13 @@ def backtrack(function, X, num_max_Y_variates=6):
         # this will cause the y's to be eliminated first
         _, rem = reduced(
             function, groebner(list(H), wrt=range_var('y', range(1, m+1, 1))))
+
         if rem == 0:
-            print [n, m] + [str_poly(h) for h in ([rem] + H)]
+            # return this
+            yield [n, m] + [str_poly(h) for h in H] + ["rem: "+str_poly(rem)]
+
+        # if extensions are no more possible
+        if n == m:
             return
 
         # idea: next expression should be chosen to move the groebner basis
@@ -78,25 +55,25 @@ def backtrack(function, X, num_max_Y_variates=6):
         # TODO: of course would be more efficient to update the groebner basis
         # rather than recomputing from scratch each time
         try:
-            for h, nn, mm in extensions(H, n, m):
+            for h, nn, mm in extensions(H, n, m, rem=rem):
                 # h  : new constraint to be enforced
                 # nn : update of number of Y variates assigned
                 # mm : update of number of Y variates in total
-                if nn < mm:
-                    search(H + [h], nn, mm)  # recurse into search
-        except TypeError:
+                for g in search(H + [h], nn, mm):  # recurse into search
+                    yield g
+        except TypeError as e:
+            print e
             pass  # for loop will barf if extensions return None
 
-    # call the search algorithm
-    # initialize with the 3 node graph y2 <- y1 -> y3
-    # y1 is already assigned ( will be assigned y1 = graph )
-    search([Poly('y1 - y2 - y3')], 1, 3)
+    # return
+    p = Poly(initial_node)
+    return search([p], 1, len(p.free_symbols))
 
-# main
-if __name__ == '__main__':
 
-    X = range_var('x', range(1, 5, 1))
-    function = Poly('y1 - ({0})'.format('+'.join(X)))
-    print "function: {}".format(str_poly(function))
-
-    backtrack(function, X)  # run the backtrack
+# time_helper
+# just run a routine once, recording its ouput and timing it
+def time_helper(routine, *args):
+    start = time.time()
+    out = routine(*args)
+    end = time.time()
+    return out, end-start
